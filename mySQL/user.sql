be10bec72e2e26310df4ff8467c73df1b17d8318
Judy Sun <ccffsun@gmail.com>
2020-03-17 20:26:32 -0700

    ConsensusOne day one

---
+++ mySQL/user.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `ConsensusOne`.`user` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `tel` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+INSERT INTO `ConsensusOne`.`user` (`id`, `name`, `email`, `tel`) VALUES ('1', 'Leo', 'leo@gmail.com', '425-777-8888');
+INSERT INTO `ConsensusOne`.`user` (`id`, `name`, `email`, `tel`) VALUES ('2', 'Judy', 'judy@gmail.com', '425-666-8888');
+INSERT INTO `ConsensusOne`.`user` (`id`, `name`, `email`, `tel`) VALUES ('3','Ricky', 'ricky@gmail.com', '425-555-8888');
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
-    public static void main (String[] args) {
-        System.out.println("Hello world");
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        String url = "jdbc:mysql://localhost:3306/ConsensusOne?serverTimezone=UTC";
+        String user = "root";
+        String password = "ccffsun86";
+        String query = "SELECT * from user";
+
+        try (Connection con = DriverManager.getConnection(url, user, password);
+             Statement st = con.createStatement();
+             ResultSet rs = st.executeQuery(query)) {
+
+            while (rs.next()) {
+                System.out.println(rs.getInt(1)+ " "+rs.getString(2));
+            }
+
+            //query = "INSERT INTO `ConsensusOne`.`user` (`id`, `name`, `email`, `tel`) VALUES ('4','Miao', 'miao@gmail.com', '425-444-8888');";
+            //query = "update user set name = 'Gaga' where name = 'Miao'";
+            query = "DELETE FROM user where id = 4";
+            st.execute(query);
+
+        } catch (SQLException ex) {
+            System.out.println(ex);
+            ex.printStackTrace();
+        }
     }
 }
