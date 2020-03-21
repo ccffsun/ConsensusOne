a7c8d3d195368388e7b2f094595a4014eadb249a
Judy Sun <ccffsun@gmail.com>
2020-03-20 23:58:27 -0700

    implement settlement

--- src/pages/helper/Common.java
+++ src/pages/helper/Common.java
@@ -1,7 +1,12 @@
 package pages.helper;
 
+import ui.Constant;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -15,6 +20,19 @@ public class Common {
     public static void PrintCurrentDateTime(){
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         Date date = new Date();
-        System.out.println(dateFormat.format(date));
+        System.out.println(Constant.ANSI_GREEN+dateFormat.format(date)+Constant.ANSI_RESET);
+    }
+
+    public static final int LEASE = 1;
+    public static final int RETURN =2;
+
+    public static LocalDate convertToLocalDate(Date dateToConvert) {
+        return Instant.ofEpochMilli(dateToConvert.getTime())
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+    }
+
+    public static int DaysBetween(Date d1, Date d2) {
+        return (int)( (d2.getTime() - d1.getTime()) / (1000 * 60 * 60 * 24));
     }
 }
--- src/pages/settlement/SettlementPage.java
+++ src/pages/settlement/SettlementPage.java
@@ -1,15 +1,161 @@
 package pages.settlement;
 
+import db.OrderDAO;
+import db.ProductDAO;
+import db.ProjectDAO;
+import dnl.utils.text.table.TextTable;
+import entity.Order;
+import entity.Product;
+import entity.Project;
 import pages.ActionPage;
+import pages.helper.Common;
+import ui.Constant;
 
-public class SettlementPage  extends ActionPage {
-    public SettlementPage (){
+import java.sql.SQLException;
+import java.util.*;
+
+public class SettlementPage extends ActionPage {
+    ProjectDAO projectDAO;
+    OrderDAO orderDAO;
+    ProductDAO productDAO;
+    double totalRent;
+    List<Object[]> tableRows;
+
+    public SettlementPage() {
         super();
         setPageName("Settlement");
     }
 
     @Override
     protected boolean runPage() {
+        projectDAO = new ProjectDAO();
+        orderDAO = new OrderDAO();
+        productDAO = new ProductDAO();
+
+        try {
+            System.out.println("Project Id:");
+            int projectId = Integer.valueOf(scanner.nextLine());
+            Project project = projectDAO.getProject(projectId);
+            if (project == null) {
+                System.out.println("Project does not exist!!!");
+                return true;
+            }
+            if (!project.getStatus().equals("closed")) {
+                System.out.println("Project is not closed yet!!!");
+                return true;
+            }
+            List<Order> orders = orderDAO.getOrdersByProjectId(projectId);
+            if (!doSettlement(orders, project.getEndDate())) {
+                System.out.println("Errors happened!");
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
         return true;
+    }
+
+    private boolean doSettlement(List<Order> orders, Date endDate) throws SQLException {
+        tableRows = new ArrayList<>();
+        totalRent = 0;
+        HashMap<Integer, Queue<Order>> orderQueueMap = new HashMap<>();
+        HashMap<Integer, Double[]> priceMap = new HashMap<>();
+        for (Order o : orders) {
+            if (o.getType() == Common.LEASE) {
+                //Queue<Order> q = map.getOrDefault(o.getProductId(), new LinkedList<>());
+                Queue<Order> q;
+                if (orderQueueMap.containsKey(o.getProductId())) {
+                    q = orderQueueMap.get(o.getProductId());
+                } else {
+                    q = new LinkedList<>();
+                    orderQueueMap.put(o.getProductId(), q);
+                }
+                q.add(o);
+            } else {
+                if (!orderQueueMap.containsKey(o.getProductId())) {
+                    System.out.println("Return product " + o.getProductId() + " not exist!!!");
+                }
+                Queue<Order> leaseQueue = orderQueueMap.get(o.getProductId());
+                int returnAmount = o.getQuantity();
+                while (returnAmount > 0) {
+                    Order leaseOrder = leaseQueue.peek();
+                    int amoutToCalculate = 0;
+                    if (leaseOrder.getQuantity() <= returnAmount) {
+                        leaseQueue.poll();
+                        amoutToCalculate = leaseOrder.getQuantity();
+                        returnAmount -= amoutToCalculate;
+                    } else {
+                        amoutToCalculate = returnAmount;
+                        leaseOrder.setQuantity(leaseOrder.getQuantity() - returnAmount);
+                        returnAmount = 0;
+                    }
+                    //Calculate money
+                    calculateRent(leaseOrder, amoutToCalculate, o.getDate(), priceMap);
+                }
+            }
+        }
+        calculateLeftOverRent(orderQueueMap, endDate, priceMap);
+        printTable();
+        return true;
+    }
+
+    private boolean calculateRent(Order leaseOrder, int quantity, Date endDate, HashMap<Integer, Double[]> priceMap) throws SQLException {
+        int days = Common.DaysBetween(leaseOrder.getDate(), endDate);
+        double firstMonPrice;
+        double unitPrice;
+        if (!validatePriceMap(leaseOrder, priceMap)) {
+            return false;
+        }
+        firstMonPrice = priceMap.get(leaseOrder.getProductId())[0];
+        unitPrice = priceMap.get(leaseOrder.getProductId())[1];
+        double rent = (firstMonPrice + Math.max(0, days - 30) * unitPrice) * quantity;
+        Object[] row = new Object[]{leaseOrder.getProductId(), days, quantity, firstMonPrice, unitPrice, rent};
+        totalRent += rent;
+        tableRows.add(row);
+        return true;
+    }
+
+    private boolean calculateLeftOverRent(HashMap<Integer, Queue<Order>> orderQueueMap, Date endDate, HashMap<Integer, Double[]> priceMap) throws SQLException {
+        for (Map.Entry<Integer, Queue<Order>> entry : orderQueueMap.entrySet()) {
+            if (!entry.getValue().isEmpty()) {
+                Queue<Order> q = entry.getValue();
+                while (!q.isEmpty()) {
+                    Order o = q.poll();
+                    if (!calculateRent(o, o.getQuantity(), endDate, priceMap)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean validatePriceMap(Order o, HashMap<Integer, Double[]> priceMap) throws SQLException {
+        if (!priceMap.containsKey(o.getProductId())) {
+            Product p = productDAO.getProduct(o.getProductId());
+            if (p == null) {
+                System.out.println("Product " + o.getProductId() + " not exist!!!");
+                return false;
+            }
+            priceMap.put(o.getProductId(), new Double[]{p.getFirstMonthPrice(), p.getPrice()});
+        }
+        return true;
+    }
+
+    private void printTable() {
+        String[] column = new String[]{"Product Id", "Rent Days", "Quantity", "First Month Price", "Unit Price", "Rent"};
+        Object[][] data = new Object[tableRows.size() + 1][column.length];
+        int i = 0;
+        for (Object[] row : tableRows) {
+            data[i] = row;
+            i++;
+        }
+        data[i] = new Object[]{"", "", "", "", "Total Rent", totalRent};
+        TextTable tt = new TextTable(column, data);
+        Common.PrintCurrentDateTime();
+        System.out.println("Rent = (FirstMonthPrice + Math.max(0, Days - 30) * UnitPrice) * Quantity");
+        tt.printTable();
+        System.out.println(Constant.SingleLine);
     }
 }
